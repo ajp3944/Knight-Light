a8f8c08bca7c799732cae9d517c64ee38ff45c79
victorz <victormzapataiii@gmail.com>
2022-05-14 14:55:51 -0400

    Main -> Cutscene -> Game
    add flow

--- knight_light/Assets/Scripts/DialogueManager.cs
+++ knight_light/Assets/Scripts/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -58,5 +59,6 @@ public class DialogueManager : MonoBehaviour
     {
         Debug.Log("The End");
         animator.SetBool("IsOpen", false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
