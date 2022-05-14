04b0796b90537d521979cce6b55a2f1ec33c6df9
puffinns <leponcee@gmail.com>
2022-05-14 02:27:18 -0400

    made a test thingy and need to fix colliders but otherwise okay

---
+++ knight_light/Assets/Scripts/TestMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour
+{
+    public TestCharacterController2D controller;
+    public float moveSpeed = 40f;
+    private float horizontalMove = 0f;
+    private bool isJumping;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        if(Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
+    }
+}
