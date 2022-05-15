c4b40d4cda21ae6fa470fd792da8a066f4b8b582
puffinns <leponcee@gmail.com>
2022-05-15 07:14:08 -0400

    general setup and animation changes

---
+++ knight_light/Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    // primary class to move/animate monster and maybe AI/pathing
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
